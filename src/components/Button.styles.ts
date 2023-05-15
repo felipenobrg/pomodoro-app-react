6befc248669de0c5b4a01cc2159d6b78cb62c62d
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-05-15 19:25:59 -0300

    colors and fonts global

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -21,7 +21,7 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
    margin: 8px;
    cursor: pointer;
 
-   background-color: ${props => props.theme.primary};
+   background-color: ${props => props.theme["green-500"]};
    color: ${props => props.theme.white}
 
    /* ${props => {
