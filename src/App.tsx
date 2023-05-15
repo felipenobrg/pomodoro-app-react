f2e9eb0fa3b7ff666c44726c6e84e5d377cd5ae2
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-05-15 19:16:37 -0300

    global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "styled-components"
 import { Button } from "./components/Button"
 import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/global"
 
 export const App = () => {
   return (
@@ -10,6 +11,8 @@ export const App = () => {
       <Button background-color="sucess" />
       <Button background-color="danger" />
       <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
