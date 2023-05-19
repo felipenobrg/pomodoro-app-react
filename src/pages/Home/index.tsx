dee89769af9dddc73d85869c6ba4fc9a74899275
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-05-19 07:44:38 -0300

    fix type errors

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,18 +15,26 @@ import {
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
-  minutesAmout: zod
+  minutesAmount: zod
     .number()
     .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
 })
 
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export const Home = () => {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
-  const handleCreateNewCycle = (data) => {}
+  const handleCreateNewCycle = (data: NewCycleFormData) => {
+    console.log(data)
+  }
 
   const task = watch('task')
   const isSubmitDisabled = !task
